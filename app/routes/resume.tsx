b8565466e227f34546787019c3f6291bf6c20055
Naveen Chandra <the.naveenchandra@gmail.com>
2026-09-08 21:38:18 +0530

    Implement ATS component for resume feedback display with score and suggestions

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -90,11 +90,12 @@ const Resume = () => {
           {feedback ? (
             <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
               <Summary feedback={feedback} />
-              {/* <ATS
+              <ATS
                 score={feedback.ATS.score || 0}
                 suggestions={feedback.ATS.tips || []}
               />
-              <Details feedback={feedback} /> */}
+
+              {/* <Details feedback={feedback} /> */}
             </div>
           ) : (
             <img src="/images/resume-scan-2.gif" className="w-full" />
